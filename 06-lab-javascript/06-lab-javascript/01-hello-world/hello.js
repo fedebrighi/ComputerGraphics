e30a5a081367aa670e54f3a0a958f5dc5d507311
Federico Brighi - federico.brighi2@studio.unibo.it <federico.brighi2@studio.unibo.it>
2024-11-29 11:35:08 +0100

    Added main folder

---
+++ 06-lab-javascript/06-lab-javascript/01-hello-world/hello.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+//const tagHello = document.getElementById("ciao");
+const tagHello = document.querySelector("#ciao");
+
+tagHello.innerHTML = "Hello World";
+
+const tagYear = document.getElementsByClassNameI("anno")[0];
